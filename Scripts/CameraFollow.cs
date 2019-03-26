b385a69b6181b590cfd33743cae7b82cf96d5d3a
JohnHP <kujoknight@gmail.com>
2019-03-25 22:36:39 -0400

    Scripts added

---
+++ Scripts/CameraFollow.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public float cameraMoveSpeed = 120.0f;
+    public GameObject cameraFollowObj;
+    private Vector3 followPossition;
+    public float clampAngle = 80.0f;
+    public float inputSensitivity = 150.0f;
+    public GameObject cameraObj;
+    public GameObject playerObj;
+    public float camDistanceXToPlayer;
+    public float camDistanceYToPlayer;
+    public float camDistanceZToPlayer;
+    public float mouseX;
+    public float mouseY;
+    public float finalInputX;
+    public float finalInputZ;
+    public float smoothX;
+    public float smoothY;
+    private float rotY = 0.0f;
+    private float rotX = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+        Vector3 rot = transform.localRotation.eulerAngles;
+        rotY = rot.y;
+        rotX = rot.x;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Set up rotation for controller sticks
+        float inputX = Input.GetAxis("RightStickHorizontal");
+        float inputZ = Input.GetAxis("RightStickVertical");
+        mouseX = Input.GetAxis("Mouse X");
+        mouseY = Input.GetAxis("Mouse Y");
+        finalInputX = inputX + mouseX;
+        finalInputZ = inputZ + mouseY;
+
+        rotY += finalInputX * inputSensitivity * Time.deltaTime;
+        rotX += finalInputZ * inputSensitivity * Time.deltaTime;
+
+        rotX = Mathf.Clamp(rotX, -clampAngle, clampAngle);
+
+        Quaternion localRotation = Quaternion.Euler(rotX, rotY, 0.0f);
+        transform.rotation = localRotation;
+	}
+
+    void LateUpdate()
+    {
+        CameraUpdater();
+    }
+
+    void CameraUpdater()
+    {
+        //Set the target object to follow
+        Transform target = cameraFollowObj.transform;
+
+        //move towards the game object that is the target
+        float step = cameraMoveSpeed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+    }
+}
---
+++ Scripts/ThirdPersonController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonController : MonoBehaviour
+{
+
+    public Transform playCamera;
+    public float speed = 10.0f;
+    public float turnSpeed = 100.0f;
+    public float rotationSpeed = 2.0f;
+    public Rigidbody rb;
+    public LayerMask ground;
+    public float groundDistance = 0.2f;
+    public float jumpHeight = 5.0f;
+    public float maxVelocityChange = 10.0f;
+    public float gravity = 5.0f;
+
+    private Vector3 dir;
+    private bool canJump = true;
+    private bool isGrounded;
+    private Transform groundCheck;
+    public bool CanMove;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        playCamera = playCamera.transform;
+        groundCheck = transform.GetChild(0);
+    }
+
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, ground, QueryTriggerInteraction.Ignore);
+    }
+
+    void FixedUpdate()
+    {
+        if (CanMove)
+        {
+            if (isGrounded)
+            {
+                // Calculate how fast we should be moving
+                Vector3 targetVelocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+                targetVelocity = playCamera.TransformDirection(targetVelocity);
+                targetVelocity *= speed;
+
+                // Apply a force that attempts to reach our target velocity
+                Vector3 velocity = rb.velocity;
+                Vector3 velocityChange = (targetVelocity - velocity);
+                velocityChange.x = Mathf.Clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange);
+                velocityChange.z = Mathf.Clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
+                velocityChange.y = 0;
+                rb.AddForce(velocityChange, ForceMode.VelocityChange);
+
+                // Jump
+                if (canJump && Input.GetButton("Jump"))
+                {
+                    rb.velocity = new Vector3(velocity.x, CalculateJumpVerticalSpeed(), velocity.z);
+                }
+            }
+
+            // We apply gravity manually for more tuning control
+            rb.AddForce(new Vector3(0, -gravity * rb.mass, 0));
+        }
+    }
+
+    float CalculateJumpVerticalSpeed()
+    {
+        // From the jump height and gravity we deduce the upwards speed 
+        // for the character to reach at the apex.
+        return Mathf.Sqrt(2 * jumpHeight * gravity);
+    }
+}
