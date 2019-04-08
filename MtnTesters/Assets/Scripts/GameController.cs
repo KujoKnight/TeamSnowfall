dab5d32960c5ba9de94b4f4db2be64d0330f6ddb
abooker32 <a.booker32@knights.ucf.edu>
2019-04-08 15:47:53 -0400

    Kill Volumes, Checkpoints, Baseline UI
    Added kill volumes, checkpoints, and a basic UI, a folder for fonts has also been added to the project.

---
+++ MtnTesters/Assets/Scripts/GameController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+    private static GameController instance;
+
+    public Vector3 lastCheckPointPos;
+    public static int playerScore;
+    public static int playerLives;
+    public static float gameTime;
+    public Text timeCounter;
+    public Text scoreCounter;
+    public Text lifeCounter;
+
+
+
+    //Makes sure the GameController game object can only have one instance of itself active in the game
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(instance);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        playerLives = 3;
+        playerScore = 0;
+        gameTime = 0;
+    }
+
+    void Update()
+    {
+        scoreCounter.text = "Score:\n" + playerScore;
+        gameTime = 1 * Time.time;
+        timeCounter.text = "Time:\n" + gameTime.ToString("0:00");
+        lifeCounter.text = "Lives: " + playerLives + " / 3";
+    }
+}
---
+++ MtnTesters/Assets/Scripts/PlayerPos.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPos : MonoBehaviour {
+
+    private GameController gc;
+
+	// Use this for initialization
+    // Spawns the player at the first checkpoint when the scene is started
+	void Start () {
+        gc = GameObject.FindGameObjectWithTag("GC").GetComponent<GameController>();
+        transform.position = gc.lastCheckPointPos;
+	}
+	
+	// Respawns the player at the most recent checkpoint when they touch a kill volume
+	void OnTriggerEnter (Collider col) {
+        if (col.gameObject.CompareTag("KillVolume"))
+        {
+            GameController.playerLives--;
+            transform.position = gc.lastCheckPointPos;
+        }
+	}
+}
