296c34fbdc08c7d62c1f37a0b2c47cf1ba935988
JohnHP <kujoknight@gmail.com>
2019-04-09 10:18:57 -0400

    Reimported pausemenu assets

---
+++ MtnTesters/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+    public static bool isGamePaused;
+    public GameObject pauseMenuUI;
+    public GameObject playerUI;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (isGamePaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        playerUI.SetActive(true);
+        Time.timeScale = 1f;
+        isGamePaused = false;
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        playerUI.SetActive(false);
+        Time.timeScale = 0f;
+        isGamePaused = true;
+    }
+}
